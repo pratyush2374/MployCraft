e3c2dc8502e90f7852cf7554a0bda270a7a4fe07
pratyush2374 <pratyushsharma2374@gmail.com>
2025-02-06 17:16:19 +0530

    Sign in directory changes

--- src/app/sign-in/page.tsx
+++ src/app/sign-in/page.tsx
@@ -1,5 +1,5 @@
 import { Metadata } from "next";
-import SignIn from "./sign-in-components/SignIn";
+import SignIn from "./SignIn";
 
 export const metadata: Metadata = {
     title: "Sign In - MployCraft",
